a0f88913c54d34c693acadac121fb0f57189df49
Liam Burnand <liamb1216@gmail.com>
2022-11-25 14:24:59 +0000

    Added better ways of creating and interacting with the environment by creating counters and chefs using an already made class, just shrank it down

---
+++ core/src/com/eng1/Objects/Counter.java
@@ -0,0 +1,29 @@
+package com.eng1.Objects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Counter extends BaseActor {
+
+    private final Rectangle bounds;
+
+    public Counter(float x, float y, int width, int height, Stage s) {
+        super(x, y, s);
+
+        bounds = new Rectangle(x, y, width, height);
+
+        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(new Color(0, 0, 1, 1));
+        pixmap.fillRectangle(0, 0, width, height);
+        setTexture(new Texture(pixmap));
+        pixmap.dispose();
+    }
+
+    public Rectangle getBounds() {
+        bounds.set(getX(), getY(), getWidth(), getHeight());
+        return bounds;
+    }
+}
