0aa7002e387a530533f4c58723c6b4e36b034a3d
Liam Burnand <liamb1216@gmail.com>
2022-11-27 20:48:38 +0000

    Simply moved Counter to the room package and removed the bounds part from Counter as that is now handled in BaseActor

--- core/src/com/eng1/room/Counter.java
+++ core/src/com/eng1/room/Counter.java
@@ -1,29 +1,20 @@
-package com.eng1.Objects;
+package com.eng1.room;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.eng1.base.BaseActor;
 
 public class Counter extends BaseActor {
-
-    private final Rectangle bounds;
 
     public Counter(float x, float y, int width, int height, Stage s) {
         super(x, y, s);
 
-        this.bounds = new Rectangle(x, y, width, height);
-
         Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
         pixmap.setColor(new Color(0, 0, 1, 1));
         pixmap.fillRectangle(0, 0, width, height);
-        setTexture(new Texture(pixmap));
+        this.setTexture(new Texture(pixmap));
         pixmap.dispose();
-    }
-
-    public Rectangle getBounds() {
-        this.bounds.set(getX(), getY(), getWidth(), getHeight());
-        return this.bounds;
     }
 }
