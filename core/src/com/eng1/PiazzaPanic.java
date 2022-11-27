38b5d622dc8a1ffbdea8a87b456cc879d3936eef
Liam Burnand <liamb1216@gmail.com>
2022-11-27 20:58:47 +0000

    Moved PiazzaPanic over from Game to BaseGame and removed some unnecessary classes which is able to be handled in BaseGame

--- core/src/com/eng1/PiazzaPanic.java
+++ core/src/com/eng1/PiazzaPanic.java
@@ -1,41 +1,22 @@
 package com.eng1;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.scenes.scene2d.*;
-import com.eng1.Objects.Mode;
+import com.eng1.base.BaseGame;
+import com.eng1.screen.MainMenuScreen;
 
-public class PiazzaPanic extends Game {
+public class PiazzaPanic extends BaseGame {
 
-	private final int width, height;
+    private final int width, height;
 
-	private final Mode mode;
+    private final Mode mode;
 
-	SpriteBatch batch;
-	BitmapFont font;
-	Stage stage;
+    public PiazzaPanic(int width, int height, Mode mode) {
+        this.width = width;
+        this.height = height;
+        this.mode = mode;
+    }
 
-	public PiazzaPanic(int width, int height, Mode mode) {
-		this.width = width;
-		this.height = height;
-		this.mode = mode;
-	}
-
-	public void create() {
-		this.batch = new SpriteBatch();
-		this.font = new BitmapFont();
-		this.stage = new Stage();
-		setScreen(new MainMenuScreen(this, this.width, this.height, this.mode));
-	}
-
-	public void render () {
-		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		this.batch.dispose();
-		this.font.dispose();
-		this.stage.dispose();
-	}
+    public void create() {
+        super.create();
+        this.setActiveScreen(new MainMenuScreen(this, this.width, this.height, this.mode));
+    }
 }
