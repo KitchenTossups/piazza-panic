02eda85713c489094b76b4e304cb8a427e29cb72
Liam Burnand <liamb1216@gmail.com>
2022-11-27 20:49:43 +0000

    Added Customer class which records the time the order was placed and what they want

---
+++ core/src/com/eng1/actor/Customer.java
@@ -0,0 +1,34 @@
+package com.eng1.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.eng1.non_actor.Recipe;
+import com.eng1.base.BaseActor;
+
+import java.util.Date;
+
+public class Customer extends BaseActor {
+
+    private final Recipe order;
+
+    private final long orderPlaced;
+
+    public Customer(float x, float y, Stage s, Recipe order) {
+        super(x, y, s);
+        this.order = order;
+
+        this.orderPlaced = new Date().getTime() + 11000;
+
+        this.setTouchable(Touchable.enabled);
+
+        this.loadTexture("button.png");
+    }
+
+    public Recipe getOrder() {
+        return this.order;
+    }
+
+    public long getOrderPlaced() {
+        return this.orderPlaced;
+    }
+}
