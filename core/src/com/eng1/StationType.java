7af38c7ab586b5d7979d5a5e306d714be8605b03
Liam Burnand <liamb1216@gmail.com>
2022-11-27 20:37:24 +0000

    This enum defines the station type and will be used for different textures and sizes

---
+++ core/src/com/eng1/StationType.java
@@ -0,0 +1,37 @@
+package com.eng1;
+
+public enum StationType {
+    FOOD_CHEST,
+    BIN,
+    CHOPPING,
+    PREP,
+    COUNTER,
+    SERVING;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case FOOD_CHEST -> {
+                return "Food Chest";
+            }
+            case BIN -> {
+                return "Bin";
+            }
+            case CHOPPING -> {
+                return "Chopping Station";
+            }
+            case PREP -> {
+                return "Preparation Station";
+            }
+            case COUNTER -> {
+                return "Counter Top";
+            }
+            case SERVING -> {
+                return "Serving Station";
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+}
