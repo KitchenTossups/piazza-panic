05661b8a56b2758cef2fd9c7adcf467640fae636
Liam Burnand <liamb1216@gmail.com>
2022-11-27 20:47:05 +0000

    Added a basic inventory for the chef and moved it to the actor package

---
+++ core/src/com/eng1/actor/Chef.java
@@ -0,0 +1,26 @@
+package com.eng1.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.eng1.non_actor.Food;
+import com.eng1.non_actor.Ingredient;
+import com.eng1.base.BaseActor;
+
+public class Chef extends BaseActor {
+
+    private Object inventoryItem;
+
+    public Chef(float x, float y, Stage s, int chefNumber) {
+        super(x, y, s);
+
+        this.loadTexture("chef" + chefNumber + ".png");
+    }
+
+    public Object getInventoryItem() {
+        return this.inventoryItem;
+    }
+
+    public void setInventoryItem(Object inventoryItem) {
+        if (inventoryItem instanceof Food || inventoryItem instanceof Ingredient)
+            this.inventoryItem = inventoryItem;
+    }
+}
