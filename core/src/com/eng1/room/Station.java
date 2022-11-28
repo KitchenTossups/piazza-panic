84985707efb4d18de7b7bd1be3d44f3bf4d9f3f6
Liam Burnand <liamb1216@gmail.com>
2022-11-28 04:48:28 +0000

    Added storage to a station as an Object, so it could be assigned to different things, in addition, changed the bin texture to bin.png

--- core/src/com/eng1/room/Station.java
+++ core/src/com/eng1/room/Station.java
@@ -8,11 +8,13 @@ public class Station extends BaseActor {
 
     private final StationType stationType;
 
+    private Object storage;
+
     public Station(float x, float y, int width, int height, float loci, Stage s, StationType stationType) {
         super(x, y, s, loci);
 
         switch (stationType) {
-            case BIN -> this.loadTexture("grey.png", width, height);
+            case BIN -> this.loadTexture("bin.png", width, height);
             case SERVING -> this.loadTexture("green.png", width, height);
             case FOOD_CHEST -> this.loadTexture("yellow.png", width, height);
             case CHOPPING -> this.loadTexture("blue.png", width, height);
@@ -25,5 +27,13 @@ public class Station extends BaseActor {
 
     public StationType getStationType() {
         return this.stationType;
+    }
+
+    public Object getStorage() {
+        return this.storage;
+    }
+
+    public void setStorage(Object storage) {
+        this.storage = storage;
     }
 }
