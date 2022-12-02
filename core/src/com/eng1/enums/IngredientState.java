1371ddf132c322189c8ca1b0175c7cff68d56db5
Liam Burnand <liamb1216@gmail.com>
2022-12-02 06:59:25 +0000

    Added half cooked so a burger can be flipped before it is ready to be served

--- core/src/com/eng1/enums/IngredientState.java
+++ core/src/com/eng1/enums/IngredientState.java
@@ -1,11 +1,13 @@
 package com.eng1.enums;
 
+@SuppressWarnings("unused")
 public enum IngredientState {
     UNPREPARED,
     PREPARED,
     UNCUT,
     CUT,
     UNCOOKED,
+    HALF_COOKED,
     COOKED,
     OVERCOOKED
 }
